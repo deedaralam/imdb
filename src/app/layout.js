3d97d91837ab22ed34d32cf2daef85135f8389f3
Deedar <deedar.a@pacsquare.com>
2023-06-01 01:20:25 +0500

    Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,19 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from "@/components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
-
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header */}
+        <Header />
+        {/* Navbar */}
+        {/* SearchBox */}
+        {children}
+      </body>
     </html>
-  )
+  );
 }
