68e3a9ec42021923708c558adc594436a60f7cb6
Deedar <deedar.a@pacsquare.com>
2023-05-31 16:15:29 +0500

    install next js

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head(){
+    return(
+        <>
+            <title>IMDB</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport"/>
+            <meta name="description" content="This is the IMDB Clone website"/>
+            <link rel="icon" href="/favicon.ico"/> 
+        </>
+    )
+}
